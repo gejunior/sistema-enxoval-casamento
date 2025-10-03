261a3ec040a25cd1d9e29ae6221fc30b87888ff0
genilson <genilson_junior14@outlook.com>
2025-10-03 13:00:53 -0300

    estruturando pastas src

--- sistema-enxoval/src/App.js
+++ sistema-enxoval/src/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { home } from './components/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Seja bem vindo!</Text>
       <StatusBar style="auto" />
     </View>
   );
